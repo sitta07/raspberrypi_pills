e61214f6c6bdfedc7463409a9bdb17c90dbc0063
sittalnwza007 <sittasahathum@gmail.com>
2025-12-08 16:20:35 +0700

    refactor : optimize code speed

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ AI_IMG_SIZE = 416
 CONF_PILL = 0.4    
 CONF_PACK = 0.7     # ลดลงนิดหน่อยเพื่อให้ Detect เจอกล่องง่ายขึ้น แล้วไปคัดที่ Logic แทน
 SCORE_PASS_PILL = 0.2
-SCORE_PASS_PACK = 0.7
+SCORE_PASS_PACK = 0.5
 
 device = torch.device("cpu")
 print(f"🚀 SYSTEM STARTING ON: {device} (Ultra-Optimized Mode)")
