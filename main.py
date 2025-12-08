ffb51c84552fc463ebb316487746890010583f9f
sittalnwza007 <sittasahathum@gmail.com>
2025-12-08 13:25:34 +0700

    feat : update Thresholds Score

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ DISPLAY_W, DISPLAY_H = 1280, 720
 AI_IMG_SIZE = 416 
 
 # Thresholds
-CONF_PILL = 0.15    
-CONF_PACK = 0.20    
-SCORE_PASS_PILL = 0.10  
+CONF_PILL = 0.7    
+CONF_PACK = 0.7    
+SCORE_PASS_PILL = 0.2  
 SCORE_PASS_PACK = 0.85  
 
 device = torch.device("cpu")
