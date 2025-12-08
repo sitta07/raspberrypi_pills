08f85bfcb4ef1267d287eb5d0aba148e483f5b93
sittalnwza007 <sittasahathum@gmail.com>
2025-12-08 16:08:40 +0700

    refactor : optimize code speed

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ AI_IMG_SIZE = 416
 
 # Thresholds
 CONF_PILL = 0.45    
-CONF_PACK = 0.7     # ลดลงนิดหน่อยเพื่อให้ Detect เจอกล่องง่ายขึ้น แล้วไปคัดที่ Logic แทน
-SCORE_PASS_PILL = 0.19
+CONF_PACK = 0.6     # ลดลงนิดหน่อยเพื่อให้ Detect เจอกล่องง่ายขึ้น แล้วไปคัดที่ Logic แทน
+SCORE_PASS_PILL = 0.18
 SCORE_PASS_PACK = 0.7
 
 device = torch.device("cpu")
